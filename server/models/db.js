ec3d59a0d0dafa9a6e5b07819477caa179e2c1fe
prasanna <lakshmiprasannakusumanchi@gmail.com>
2024-04-23 19:30:56 +0530

    changes made

--- server/models/db.js
+++ server/models/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+
 // Allow Promises
 mongoose.Promise = global.Promise;
-// Connection
-mongoose.connect('mongodb://0.0.0.0:27017/db_test', { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true});
-// Validation
-mongoose.connection
-  .once('open', () => console.log('Connected to the database!'))
-  .on('error', err => console.log('Error with the database!', err));
+
+// MongoDB Connection
+mongoose.connect('mongodb://0.0.0.0:27017/db_test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Connection Event Handlers
+mongoose.connection.once('open', () => {
+  console.log('Connected to the database!');
+}).on('error', err => {
+  console.error('Error with the database:', err);
+});
